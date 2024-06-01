7655ab54b33a48a027e01f1e93728d9af639b9ca
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-01 17:23:27 -0400

    1.2 Add Constants

---
+++ src/main/java/com/jqtools/powersql/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.jqtools.powersql.constants;
+
+public class Constants {
+	public static final String TITLE = "Power SQL";
+	public static final String ROOT = "Databases";
+
+	public static final String PANEL_CENTER = "Center";
+	public static final int DIVIDER_SIZE = 2;
+	public static final int LEFT_WIDE = 300;
+
+}
