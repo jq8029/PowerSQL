7d132f38339c7065a9f2c84fb8717824cd039cf0
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-03 21:06:51 -0400

    add split panel top size

--- src/main/java/com/jqtools/powersql/constants/Constants.java
+++ src/main/java/com/jqtools/powersql/constants/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 	public static final String PANEL_CENTER = "Center";
 	public static final int DIVIDER_SIZE = 2;
 	public static final int LEFT_WIDE = 300;
+	public static final int TOP_WIDE = 320;
 
 	public static final String TAB_DATA = "Data";
 	public static final String TAB_SQL = "SQL";
