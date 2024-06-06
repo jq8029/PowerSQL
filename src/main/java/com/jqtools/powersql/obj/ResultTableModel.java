64032ed6db5a184ca60a96a223ea5dab541eaf50
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-05 20:26:12 -0400

    add ResultTableModel

---
+++ src/main/java/com/jqtools/powersql/obj/ResultTableModel.java
@@ -0,0 +1,27 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ResultTableModel extends AbstractTableModel {
+
+	private static final long serialVersionUID = -8670835121747587879L;
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
