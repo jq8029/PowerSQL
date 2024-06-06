35cbac30a9a63fc1c19b9e62ae14a06c33921155
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-05 20:27:26 -0400

    add table data to tablemodel

--- src/main/java/com/jqtools/powersql/obj/ResultTableModel.java
+++ src/main/java/com/jqtools/powersql/obj/ResultTableModel.java
@@ -1,10 +1,16 @@
 package com.jqtools.powersql.obj;
 
+import java.util.Vector;
+
 import javax.swing.table.AbstractTableModel;
 
 public class ResultTableModel extends AbstractTableModel {
 
 	private static final long serialVersionUID = -8670835121747587879L;
+
+	private Vector<Object[]> data = null;
+	private String[] colNames = null;
+	private int[] colTypes = null;
 
 	@Override
 	public int getRowCount() {
