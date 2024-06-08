50364e421339e0d75ba7fd60aad6b8e7a70e6468
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-08 15:06:00 -0400

    implement execute method

--- src/main/java/com/jqtools/powersql/utils/ExecuteSQL.java
+++ src/main/java/com/jqtools/powersql/utils/ExecuteSQL.java
@@ -1,9 +1,43 @@
 package com.jqtools.powersql.utils;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.jqtools.powersql.log.MessageLogger;
 import com.jqtools.powersql.obj.Session;
 
 public class ExecuteSQL {
 	public static boolean executeSQL(Session session, String sql) {
+		PreparedStatement stat = null;
+		ResultSet result = null;
+
+		try {
+			stat = session.getConnection().prepareStatement(sql);
+			if (stat.execute()) {
+				result = stat.getResultSet();
+				int colNum = -1;
+				ArrayList<Object[]> data = new ArrayList<Object[]>();
+				Object objs[] = null;
+				if (result != null) {
+					while (result.next()) {
+						if (colNum < 0) {
+							colNum = result.getMetaData().getColumnCount();
+						}
+
+						objs = new Object[colNum];
+						for (int i = 0; i < colNum; i++) {
+							objs[i] = result.getObject(i);
+						}
+						data.add(objs);
+					}
+				}
+			}
+		} catch (Exception e) {
+			MessageLogger.error(e);
+			;
+		}
+
 		return true;
 	}
 }
