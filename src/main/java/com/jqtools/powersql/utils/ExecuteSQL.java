f1bdac98143d323f5634d3677ef3e54a94bc256a
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-08 14:41:28 -0400

    add ExecuteSQL

---
+++ src/main/java/com/jqtools/powersql/utils/ExecuteSQL.java
@@ -0,0 +1,9 @@
+package com.jqtools.powersql.utils;
+
+import com.jqtools.powersql.obj.Session;
+
+public class ExecuteSQL {
+	public static boolean executeSQL(Session session, String sql) {
+		return true;
+	}
+}
