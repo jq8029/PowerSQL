f2a035a8ca6c5dbd9ce22d7becf9ae7ae2c90810
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-12 21:06:47 -0400

    add tablemodel to session

--- src/main/java/com/jqtools/powersql/obj/Session.java
+++ src/main/java/com/jqtools/powersql/obj/Session.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 public class Session {
 	private Connection connection = null;
 	private DatabaseInfo dbInfo = null;
+	private ResultTableModel tableModel = null;
 
 	/**
 	 * @return the connection
@@ -32,6 +33,20 @@ public class Session {
 	 */
 	public void setDbInfo(DatabaseInfo dbInfo) {
 		this.dbInfo = dbInfo;
+	}
+
+	/**
+	 * @return the tableModel
+	 */
+	public ResultTableModel getTableModel() {
+		return tableModel;
+	}
+
+	/**
+	 * @param tableModel the tableModel to set
+	 */
+	public void setTableModel(ResultTableModel tableModel) {
+		this.tableModel = tableModel;
 	}
 
 }
