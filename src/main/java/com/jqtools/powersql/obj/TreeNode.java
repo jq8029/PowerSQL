2fec4d3651952ee3a9dae2631fe7b6fc8c25a576
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-14 21:01:28 -0400

    add removeFromParent in TreeNode

--- src/main/java/com/jqtools/powersql/obj/TreeNode.java
+++ src/main/java/com/jqtools/powersql/obj/TreeNode.java
@@ -1,6 +1,8 @@
 package com.jqtools.powersql.obj;
 
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 public class TreeNode extends DefaultMutableTreeNode {
 	/**
@@ -8,4 +10,8 @@ public class TreeNode extends DefaultMutableTreeNode {
 	 */
 	private static final long serialVersionUID = -5351735513734659428L;
 
+	public void removeFromParent(JTree tree) {
+		DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
+		model.removeNodeFromParent(this);
+	}
 }
