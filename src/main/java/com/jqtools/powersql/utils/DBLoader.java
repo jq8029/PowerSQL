8a8ffb64a7c50745fe6db7b23f236b3efe6711ef
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-14 21:02:46 -0400

    add DBLoader

---
+++ src/main/java/com/jqtools/powersql/utils/DBLoader.java
@@ -0,0 +1,10 @@
+package com.jqtools.powersql.utils;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class DBLoader {
+	public static void loadDBNode(DefaultMutableTreeNode root) {
+
+	}
+
+}
