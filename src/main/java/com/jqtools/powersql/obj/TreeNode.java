2aef7a3d2b9e54b7a9bbb391d59ce5a1f8a6d2ed
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-14 21:00:46 -0400

    add TreeNode

---
+++ src/main/java/com/jqtools/powersql/obj/TreeNode.java
@@ -0,0 +1,11 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class TreeNode extends DefaultMutableTreeNode {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5351735513734659428L;
+
+}
