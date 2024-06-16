47d282d69262ddb66fce88cbf2a66c062c678738
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-15 22:30:29 -0400

    Update H2Data.java

--- src/main/java/com/jqtools/powersql/db/H2Data.java
+++ src/main/java/com/jqtools/powersql/db/H2Data.java
@@ -1,18 +1,22 @@
 package com.jqtools.powersql.db;
 
-public class H2Data {
+public class H2Data extends DatabaseData {
+	@Override
 	public String getCatalogAllSQL() {
 		return null;
 	}
 
+	@Override
 	public String getSchemaAllSQL() {
 		return null;
 	}
 
+	@Override
 	public String getTableSchemaSQL() {
 		return null;
 	}
 
+	@Override
 	public String getViewSchemaSQL() {
 		return null;
 	}
