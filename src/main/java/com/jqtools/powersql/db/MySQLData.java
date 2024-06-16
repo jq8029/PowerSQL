45840ad478b40be77cacbc586bb50e5bb9f9c01d
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-15 22:26:26 -0400

    Update MySQLData.java

--- src/main/java/com/jqtools/powersql/db/MySQLData.java
+++ src/main/java/com/jqtools/powersql/db/MySQLData.java
@@ -7,10 +7,12 @@ public class MySQLData extends DatabaseData {
 		return "SELECT CATALOG_NAME as MY_CATALOG, SCHEMA_NAME as MY_SCHEMA FROM information_schema.SCHEMATA order by SCHEMA_NAME";
 	}
 
+	@Override
 	public String getTableSchemaSQL() {
 		return "SELECT * FROM information_schema.TABLES where TABLE_SCHEMA = ? and TABLE_TYPE <> 'VIEW' and TABLE_TYPE <> 'SYSTEM VIEW'";
 	}
 
+	@Override
 	public String getViewSchemaSQL() {
 		return "SELECT * FROM information_schema.TABLES where TABLE_SCHEMA = ? and (TABLE_TYPE = 'VIEW' or TABLE_TYPE = 'SYSTEM VIEW')";
 	}
