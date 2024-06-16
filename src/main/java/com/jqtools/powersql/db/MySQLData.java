951fbea6f3dc47f94d932d54e58000dcd66f41aa
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-15 22:15:06 -0400

    Create MySQLData.java

---
+++ src/main/java/com/jqtools/powersql/db/MySQLData.java
@@ -0,0 +1,40 @@
+package com.jqtools.powersql.db;
+
+public class MySQLData {
+	private String catalogAllSQL = null;
+	private String schemaAllSQL = null;
+	private String tableSchemaSQL = null;
+	private String viewSchemaSQL = null;
+
+	public String getCatalogAllSQL() {
+		return catalogAllSQL;
+	}
+
+	public void setCatalogAllSQL(String catalogAllSQL) {
+		this.catalogAllSQL = catalogAllSQL;
+	}
+
+	public String getSchemaAllSQL() {
+		return schemaAllSQL;
+	}
+
+	public void setSchemaAllSQL(String schemaAllSQL) {
+		this.schemaAllSQL = schemaAllSQL;
+	}
+
+	public String getTableSchemaSQL() {
+		return tableSchemaSQL;
+	}
+
+	public void setTableSchemaSQL(String tableSchemaSQL) {
+		this.tableSchemaSQL = tableSchemaSQL;
+	}
+
+	public String getViewSchemaSQL() {
+		return viewSchemaSQL;
+	}
+
+	public void setViewSchemaSQL(String viewSchemaSQL) {
+		this.viewSchemaSQL = viewSchemaSQL;
+	}
+}
