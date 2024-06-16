e86bc6a2ab146c44b00a730584e4595377a1670d
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-15 22:31:32 -0400

    Update H2Data.java

--- src/main/java/com/jqtools/powersql/db/H2Data.java
+++ src/main/java/com/jqtools/powersql/db/H2Data.java
@@ -3,7 +3,7 @@ package com.jqtools.powersql.db;
 public class H2Data extends DatabaseData {
 	@Override
 	public String getCatalogAllSQL() {
-		return null;
+		return "SELECT distinct CATALOG_NAME as MY_CATALOG FROM information_schema.CATALOGS order by MY_CATALOG";
 	}
 
 	@Override
