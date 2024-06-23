a07a4ca0b94a05a63a853fb07f651b5e3b9bda09
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-23 17:58:10 -0400

    Update DBLoader.java

--- src/main/java/com/jqtools/powersql/utils/DBLoader.java
+++ src/main/java/com/jqtools/powersql/utils/DBLoader.java
@@ -1,10 +1,37 @@
 package com.jqtools.powersql.utils;
 
+import java.sql.Connection;
+
+import com.jqtools.powersql.log.MessageLogger;
+import com.jqtools.powersql.obj.Info;
 import com.jqtools.powersql.obj.Session;
 
 public class DBLoader {
-	public static void loadDBNode(Session session) {
+	public static boolean loadDBNode(Session session) {
+		if (session == null || session.getDbInfo() == null || session.getDbNode() == null) {
+			return false;
+		}
 
+		Connection conn = null;
+
+		try {
+
+			conn = DBTools.getConnection(session.getDbInfo());
+			Info info = session.getDbNode().getInfo();
+		} catch (Throwable e) {
+			MessageLogger.error(e);
+
+			return false;
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+
+		return true;
 	}
 
 }
