0909eb735bb454d483e2a78790572cbf501790f1
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-23 17:48:42 -0400

    Update DBLoader.java

--- src/main/java/com/jqtools/powersql/utils/DBLoader.java
+++ src/main/java/com/jqtools/powersql/utils/DBLoader.java
@@ -1,9 +1,9 @@
 package com.jqtools.powersql.utils;
 
-import com.jqtools.powersql.obj.TreeNode;
+import com.jqtools.powersql.obj.Session;
 
 public class DBLoader {
-	public static void loadDBNode(TreeNode root) {
+	public static void loadDBNode(Session session) {
 
 	}
 
