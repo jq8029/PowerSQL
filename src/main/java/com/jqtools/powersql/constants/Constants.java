4921d5ba118319753c5f4714deed4d5ce0c4298f
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-06-29 12:00:48 -0400

    Update Constants.java

--- src/main/java/com/jqtools/powersql/constants/Constants.java
+++ src/main/java/com/jqtools/powersql/constants/Constants.java
@@ -37,5 +37,5 @@ public class Constants {
 	public static final String MY_VIEW = "MY_VIEW";
 
 	public static final String DB_H2 = "H2";
-	public static final String DB_MySQL = "MySQL";
+	public static final String DB_MYSQL = "MySQL";
 }
