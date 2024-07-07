e323a1f164a8e56d3319c7a970fa1cc1aeb59403
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-06 21:06:14 -0400

    Update DatabaseData.java

--- src/main/java/com/jqtools/powersql/db/DatabaseData.java
+++ src/main/java/com/jqtools/powersql/db/DatabaseData.java
@@ -1,6 +1,11 @@
 package com.jqtools.powersql.db;
 
 public class DatabaseData {
+	public static final String MY_CATALOG = "MY_CATALOG";
+	public static final String MY_SCHEMA = "MY_SCHEMA";
+	public static final String MY_TABLE = "MY_TABLE";
+	public static final String MY_VIEW = "MY_VIEW";
+
 	public String getCatalogAllSQL() {
 		return null;
 	}
@@ -15,5 +20,21 @@ public class DatabaseData {
 
 	public String getViewSchemaSQL() {
 		return null;
+	}
+
+	public String getCatalogName() {
+		return MY_CATALOG;
+	}
+
+	public String getSchemaName() {
+		return MY_SCHEMA;
+	}
+
+	public String getTableName() {
+		return MY_TABLE;
+	}
+
+	public String getViewName() {
+		return MY_VIEW;
 	}
 }
