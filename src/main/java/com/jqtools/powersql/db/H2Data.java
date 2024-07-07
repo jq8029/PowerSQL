9e55332fa04355fe0aa4a27473a8a0f35a8a6c03
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-06 21:06:09 -0400

    Update H2Data.java

--- src/main/java/com/jqtools/powersql/db/H2Data.java
+++ src/main/java/com/jqtools/powersql/db/H2Data.java
@@ -3,12 +3,12 @@ package com.jqtools.powersql.db;
 public class H2Data extends DatabaseData {
 	@Override
 	public String getCatalogAllSQL() {
-		return "SELECT distinct CATALOG_NAME as MY_CATALOG FROM information_schema.CATALOGS order by MY_CATALOG";
+		return "SELECT distinct CATALOG_NAME as MY_CATALOG FROM information_schema.CATALOGS order by CATALOG_NAME";
 	}
 
 	@Override
 	public String getSchemaAllSQL() {
-		return "SELECT CATALOG_NAME as MY_CATALOG, SCHEMA_NAME as MY_SCHEMA FROM information_schema.SCHEMATA order by MY_SCHEMA";
+		return "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA where CATALOG_NAME = ? order by SCHEMA_NAME";
 	}
 
 	@Override
@@ -20,4 +20,9 @@ public class H2Data extends DatabaseData {
 	public String getViewSchemaSQL() {
 		return "SELECT TABLE_NAME as MY_VIEW FROM information_schema.VIEWS where TABLE_SCHEMA = ? order by TABLE_NAME";
 	}
+
+	public String getCatalogName() {
+		return "CATALOG_NAME";
+	}
+
 }
