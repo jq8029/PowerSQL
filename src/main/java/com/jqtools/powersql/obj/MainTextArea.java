3d903fea07ae686b1a6611c51b61671cfb7ede1c
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-15 21:34:38 -0400

    Create MainTextArea.java

---
+++ src/main/java/com/jqtools/powersql/obj/MainTextArea.java
@@ -0,0 +1,12 @@
+package com.jqtools.powersql.obj;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+
+public class MainTextArea extends RSyntaxTextArea {
+
+	private static final long serialVersionUID = 1064609692697845846L;
+
+	public MainTextArea() {
+
+	}
+}
