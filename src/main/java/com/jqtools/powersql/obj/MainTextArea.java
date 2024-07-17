bd3d924aaca22cae141f6c423c3a3250a1d66c8b
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-16 21:20:34 -0400

    Update MainTextArea.java

--- src/main/java/com/jqtools/powersql/obj/MainTextArea.java
+++ src/main/java/com/jqtools/powersql/obj/MainTextArea.java
@@ -1,8 +1,11 @@
 package com.jqtools.powersql.obj;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 
@@ -16,7 +19,7 @@ public class MainTextArea extends RSyntaxTextArea {
 	public MainTextArea() {
 		textArea = this;
 
-		// register key to execute current sql
+		// register key (Ctrl + Enter) to execute current sql
 		getActionMap().put(Constants.KEY_EXEC_CURRENT_SQL, new AbstractAction(Constants.KEY_EXEC_CURRENT_SQL) {
 			private static final long serialVersionUID = 4711206524548141227L;
 
@@ -29,6 +32,8 @@ public class MainTextArea extends RSyntaxTextArea {
 				}
 			}
 		});
+		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.CTRL_DOWN_MASK),
+				Constants.KEY_EXEC_CURRENT_SQL);
 
 	}
 }
