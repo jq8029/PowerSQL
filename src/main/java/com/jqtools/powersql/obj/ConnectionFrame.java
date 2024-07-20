1bd7b694bcbc1efa441f3302c6f65d6d3d76a06f
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-20 10:59:36 -0400

    Create ConnectionFrame.java

---
+++ src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
@@ -0,0 +1,13 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.JFrame;
+
+public class ConnectionFrame extends JFrame {
+	private static final long serialVersionUID = 4752644080497862101L;
+
+	private static ConnectionFrame conFrame = null;
+
+	private ConnectionFrame() {
+
+	}
+}
