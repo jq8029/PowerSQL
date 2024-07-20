f8eff41aabfce20899e7230b2acfe0d74e9d9625
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-20 10:59:59 -0400

    Update ConnectionFrame.java

--- src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
+++ src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
@@ -5,6 +5,13 @@ import javax.swing.JFrame;
 public class ConnectionFrame extends JFrame {
 	private static final long serialVersionUID = 4752644080497862101L;
 
+	public static final int INDEX_NAME = 0;
+	public static final int INDEX_USER = 1;
+	public static final int INDEX_PWD = 2;
+	public static final int INDEX_URL = 3;
+	public static final int INDEX_DRIVER = 4;
+	public static final int INDEX_JARS = 5;
+
 	private static ConnectionFrame conFrame = null;
 
 	private ConnectionFrame() {
