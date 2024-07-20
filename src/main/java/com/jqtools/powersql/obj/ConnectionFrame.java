1119781f313419d0caf51987f82a5fee4c105291
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-20 11:00:39 -0400

    Update ConnectionFrame.java

--- src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
+++ src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
@@ -1,6 +1,9 @@
 package com.jqtools.powersql.obj;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import com.jqtools.powersql.constants.Constants;
 
@@ -17,6 +20,12 @@ public class ConnectionFrame extends JFrame {
 	private static final String TEXT[] = { Constants.CONNECTION_NAME, Constants.CONNECTION_USER,
 			Constants.CONNECTION_PWD, Constants.CONNECTION_URL, Constants.CONNECTION_DRIVER_NAME,
 			Constants.CONNECTION_DRIVER_JAR };
+
+	private JLabel names[] = new JLabel[TEXT.length];
+	private JTextField values[] = new JTextField[TEXT.length];
+	private JButton addButton = new JButton(Constants.BUTTON_ADD);
+	private JButton saveButton = new JButton(Constants.BUTTON_SAVE);
+	private JButton closeButton = new JButton(Constants.BUTTON_CLOSE);
 
 	private static ConnectionFrame conFrame = null;
 
