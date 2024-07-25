2d95d7a09d7cbdc4a4b010dbabf11cb276155f90
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-07-24 20:33:33 -0400

    Update ConnectionFrame.java

--- src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
+++ src/main/java/com/jqtools/powersql/obj/ConnectionFrame.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.sql.Connection;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -127,6 +128,15 @@ public class ConnectionFrame extends JFrame {
 		dbInfo.setUrl(getValue(INDEX_URL));
 		dbInfo.setDriverName(getValue(INDEX_DRIVER));
 		dbInfo.setJarFiles(getValue(INDEX_JARS));
+
+		Connection conn = null;
+		String error = null;
+
+		try {
+			conn = DBTools.createConnection(dbInfo);
+		} catch (Exception ex) {
+			error = ex.getMessage();
+		}
 
 	}
 
