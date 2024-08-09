484cae8f66e2f4785a84ba6aefa42d161906423d
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-08-08 20:29:57 -0400

    Create TreePopMenu.java

---
+++ src/main/java/com/jqtools/powersql/obj/TreePopMenu.java
@@ -0,0 +1,28 @@
+package com.jqtools.powersql.obj;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JPopupMenu;
+
+public class TreePopMenu extends JPopupMenu implements ActionListener, ItemListener {
+
+	public TreePopMenu() {
+		super("");
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
