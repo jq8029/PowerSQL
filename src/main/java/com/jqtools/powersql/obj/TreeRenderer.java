5d26028f050161ba29699e29e2e77cc2340bc89a
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-08-23 21:52:19 -0400

    Create TreeRenderer.java

---
+++ src/main/java/com/jqtools/powersql/obj/TreeRenderer.java
@@ -0,0 +1,9 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+public class TreeRenderer extends DefaultTreeCellRenderer {
+
+	private static final long serialVersionUID = -7708912108715261741L;
+
+}
