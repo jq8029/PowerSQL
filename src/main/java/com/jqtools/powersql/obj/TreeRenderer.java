6c2c53e6b1cbf4d2607d23455a54fc566fcd8fd8
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-08-23 22:01:02 -0400

    Update TreeRenderer.java

--- src/main/java/com/jqtools/powersql/obj/TreeRenderer.java
+++ src/main/java/com/jqtools/powersql/obj/TreeRenderer.java
@@ -6,6 +6,8 @@ import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
+import com.jqtools.powersql.constants.Constants;
+
 public class TreeRenderer extends DefaultTreeCellRenderer {
 
 	private static final long serialVersionUID = -7708912108715261741L;
@@ -28,6 +30,25 @@ public class TreeRenderer extends DefaultTreeCellRenderer {
 			// cast to TreeNode
 			TreeNode node = (TreeNode) value;
 
+			if (node.getInfo().getNodeType() == Constants.NODE_CONNECTIONS) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_CONNECTION) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_CATALOG) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_CATALOG) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_SCHEMA) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_TABLES) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_TABLE) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_VIEWS) {
+
+			} else if (node.getInfo().getNodeType() == Constants.NODE_VIEW) {
+
+			}
 		}
 
 		return labelNull;
