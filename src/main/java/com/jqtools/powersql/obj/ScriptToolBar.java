d137e89fecd7dfa96b2c496f52490e914c2cb348
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-08-29 21:09:50 -0400

    Create ScriptToolBar.java

---
+++ src/main/java/com/jqtools/powersql/obj/ScriptToolBar.java
@@ -0,0 +1,18 @@
+package com.jqtools.powersql.obj;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JToolBar;
+
+public class ScriptToolBar extends JToolBar implements ActionListener {
+
+	public ScriptToolBar() {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+
+}
