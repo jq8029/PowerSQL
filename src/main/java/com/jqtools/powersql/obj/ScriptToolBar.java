d5052cde344659c880b0c439026e92da83f19645
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-08-30 20:39:31 -0400

    Update ScriptToolBar.java

--- src/main/java/com/jqtools/powersql/obj/ScriptToolBar.java
+++ src/main/java/com/jqtools/powersql/obj/ScriptToolBar.java
@@ -3,13 +3,31 @@ package com.jqtools.powersql.obj;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JToolBar;
+
+import com.jqtools.powersql.constants.Constants;
 
 public class ScriptToolBar extends JToolBar implements ActionListener {
 
 	private static final long serialVersionUID = 2325100178568484815L;
+	private JButton buttons[] = new JButton[Constants.TEXTS.length];
 
 	public ScriptToolBar() {
+		int space = Constants.SPACE_03;
+		int count = 0;
+
+		for (int i = 0; i < Constants.TEXTS.length; i++) {
+			buttons[i] = new JButton(new ImageIcon(Constants.ICON_PATH + Constants.TEXTS[i]));
+			buttons[i].setActionCommand(Constants.TEXTS[i]);
+			buttons[i].addActionListener(this);
+			buttons[i].setToolTipText(Constants.TEXTS[i]);
+
+			buttons[i].setBounds(Constants.SPACE_23 * count + space, Constants.SPACE_02, Constants.SPACE_21,
+					Constants.SPACE_21);
+			add(buttons[i]);
+		}
 	}
 
 	@Override
