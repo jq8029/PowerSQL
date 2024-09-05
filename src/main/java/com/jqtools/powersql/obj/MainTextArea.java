4e6f54869b4d3eeea4b1ec318b9b5a3899976cfa
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-04 20:47:37 -0400

    Update MainTextArea.java

--- src/main/java/com/jqtools/powersql/obj/MainTextArea.java
+++ src/main/java/com/jqtools/powersql/obj/MainTextArea.java
@@ -28,43 +28,47 @@ public class MainTextArea extends RSyntaxTextArea {
 			private static final long serialVersionUID = 4711206524548141227L;
 
 			public void actionPerformed(ActionEvent evt) {
-				try {
-					// skip it if there is no session
-					if (session == null)
-						return;
-
-					// skip it if no text in the textArea
-					if (textArea != null && textArea.getText() != null && textArea.getText().trim().length() > 0) {
-						String sql = textArea.getSelectedText();
-
-						if (sql == null || sql.length() == 0) {
-							int pos = textArea.getCaretPosition();
-							int start = textArea.getText().lastIndexOf(";", pos);
-							int end = textArea.getText().indexOf(";", pos);
-							if (start < 0) {
-								start = 0;
-							} else {
-								start = start + 1;
-							}
-							if (end < 0)
-								end = textArea.getText().length();
-
-							if (end <= start) {
-								return;
-							} else {
-								sql = textArea.getText().substring(start, end);
-							}
-						}
-
-						ExecuteSQL.execute(session, sql, resultScroll);
-					}
-				} catch (Exception e) {
-				}
+				executeSQL();
 			}
 		});
 		getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.CTRL_DOWN_MASK),
 				Constants.KEY_EXEC_CURRENT_SQL);
 
+	}
+
+	public void executeSQL() {
+		try {
+			// skip it if there is no session
+			if (session == null)
+				return;
+
+			// skip it if no text in the textArea
+			if (textArea != null && textArea.getText() != null && textArea.getText().trim().length() > 0) {
+				String sql = textArea.getSelectedText();
+
+				if (sql == null || sql.length() == 0) {
+					int pos = textArea.getCaretPosition();
+					int start = textArea.getText().lastIndexOf(";", pos);
+					int end = textArea.getText().indexOf(";", pos);
+					if (start < 0) {
+						start = 0;
+					} else {
+						start = start + 1;
+					}
+					if (end < 0)
+						end = textArea.getText().length();
+
+					if (end <= start) {
+						return;
+					} else {
+						sql = textArea.getText().substring(start, end);
+					}
+				}
+
+				ExecuteSQL.execute(session, sql, resultScroll);
+			}
+		} catch (Exception e) {
+		}
 	}
 
 	public void setSession(Session session) {
