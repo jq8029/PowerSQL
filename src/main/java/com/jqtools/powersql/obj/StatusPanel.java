2a8ef91a546db16730723b7eabf49bf72424918e
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-07 20:07:32 -0400

    Update StatusPanel.java

--- src/main/java/com/jqtools/powersql/obj/StatusPanel.java
+++ src/main/java/com/jqtools/powersql/obj/StatusPanel.java
@@ -7,12 +7,12 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class StatusBarPanel extends JPanel {
+public class StatusPanel extends JPanel {
 	private static final long serialVersionUID = -2647008365234019097L;
 
 	private JLabel posLabel = new JLabel("    Pos : 1        Row : 1,  Col : 1  ");
 
-	public StatusBarPanel() {
+	public StatusPanel() {
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
