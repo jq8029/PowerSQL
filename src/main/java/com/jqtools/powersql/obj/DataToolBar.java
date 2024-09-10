5c3483686a15332c6272bd9d651eb1ca3c1f81d1
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-09 20:48:48 -0400

    Update DataToolBar.java

--- src/main/java/com/jqtools/powersql/obj/DataToolBar.java
+++ src/main/java/com/jqtools/powersql/obj/DataToolBar.java
@@ -9,6 +9,7 @@ import javax.swing.JToolBar;
 
 import com.jqtools.powersql.constants.Constants;
 import com.jqtools.powersql.log.MessageLogger;
+import com.jqtools.powersql.utils.Tools;
 
 public class DataToolBar extends JToolBar implements ActionListener {
 
@@ -16,6 +17,17 @@ public class DataToolBar extends JToolBar implements ActionListener {
 	private JButton buttons[] = new JButton[Constants.DATA_TEXTS.length];
 
 	public DataToolBar() {
+		int space = Constants.SPACE_03;
+		int count = 0;
+
+		for (int i = 0; i < Constants.SQL_TEXTS.length; i++) {
+			buttons[i] = Tools.getImageButton(Constants.SQL_TEXTS[i], Constants.SQL_ENABLES[i]);
+			buttons[i].addActionListener(this);
+
+			buttons[i].setBounds(Constants.SPACE_23 * count + space, Constants.SPACE_02, Constants.SPACE_21,
+					Constants.SPACE_21);
+			add(buttons[i]);
+		}
 
 		this.setForeground(Color.BLACK);
 	}
