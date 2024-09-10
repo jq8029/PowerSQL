fbad25531374f2e0c85c71d988e786088cec84cc
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-09 20:55:31 -0400

    Create DataTable.java

---
+++ src/main/java/com/jqtools/powersql/obj/DataTable.java
@@ -0,0 +1,9 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.JTable;
+
+public class DataTable extends JTable {
+
+	private static final long serialVersionUID = -7489359896418415220L;
+
+}
