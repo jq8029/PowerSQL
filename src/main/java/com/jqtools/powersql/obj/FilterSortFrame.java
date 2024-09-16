121f882db0fb02504f404363e3614248d5b6c808
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-15 21:07:12 -0400

    Create FilterSortFrame.java

---
+++ src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
@@ -0,0 +1,13 @@
+package com.jqtools.powersql.obj;
+
+import javax.swing.JFrame;
+
+public class FilterSortFrame extends JFrame {
+
+	private static final long serialVersionUID = 2086152060767330545L;
+
+	public FilterSortFrame() {
+		super();
+	}
+
+}
