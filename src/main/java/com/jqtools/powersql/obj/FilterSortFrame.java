8b4b361ac03fd09f7c6887f3a7f0bc3db7ede03c
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-16 20:15:06 -0400

    Update FilterSortFrame.java

--- src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
+++ src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
@@ -2,12 +2,20 @@ package com.jqtools.powersql.obj;
 
 import javax.swing.JFrame;
 
+import com.jqtools.powersql.constants.Constants;
+
 public class FilterSortFrame extends JFrame {
 
 	private static final long serialVersionUID = 2086152060767330545L;
 
 	public FilterSortFrame() {
 		super();
+
+		this.setTitle(Constants.TITLE_FILTER_SORT);
+		this.getContentPane().setLayout(null);
+
+		this.pack();
+		setLocationRelativeTo(this);
 	}
 
 }
