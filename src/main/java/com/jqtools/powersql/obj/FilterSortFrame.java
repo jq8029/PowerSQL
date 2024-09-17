e040ccad50ef33d83c64677bc6029951fbcc912d
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-16 20:19:32 -0400

    Update FilterSortFrame.java

--- src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
+++ src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
@@ -1,6 +1,7 @@
 package com.jqtools.powersql.obj;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.jqtools.powersql.constants.Constants;
 
@@ -13,6 +14,19 @@ public class FilterSortFrame extends JFrame {
 
 		this.setTitle(Constants.TITLE_FILTER_SORT);
 		this.getContentPane().setLayout(null);
+
+		int height = 16;
+		int x1 = 15;
+		int y1 = 20;
+
+		// add filter panel
+		JPanel filterPanel = new JPanel(null);
+
+		// add sort panel
+		JPanel sortPanel = new JPanel(null);
+
+		this.getContentPane().add(filterPanel);
+		this.getContentPane().add(sortPanel);
 
 		this.pack();
 		setLocationRelativeTo(this);
