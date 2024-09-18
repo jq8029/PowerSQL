1566e4f9f64c39e4b524cfe612dd1e80576433bb
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-17 22:08:02 -0400

    Update FilterSortFrame.java

--- src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
+++ src/main/java/com/jqtools/powersql/obj/FilterSortFrame.java
@@ -1,14 +1,25 @@
 package com.jqtools.powersql.obj;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 
 import com.jqtools.powersql.constants.Constants;
 
 public class FilterSortFrame extends JFrame {
 
 	private static final long serialVersionUID = 2086152060767330545L;
+
+	private RSyntaxTextArea filterArea = new RSyntaxTextArea();
+	private JComboBox<String> filterColBox = new JComboBox<String>();
+	private JComboBox<String> condBox = new JComboBox<String>(Constants.CONDITIONS);
+	private JComboBox<String> andOrBox = new JComboBox<String>(Constants.AND_OR);
+	private JComboBox<String> ascDescBox = new JComboBox<String>(Constants.ORDERS);
+	private JTextField filterField = new JTextField();
 
 	public FilterSortFrame() {
 		super();
