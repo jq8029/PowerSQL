27e3721dae6d0a84bd8105e94454e4bc08cc8b29
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-17 22:10:00 -0400

    Update Constants.java

--- src/main/java/com/jqtools/powersql/constants/Constants.java
+++ src/main/java/com/jqtools/powersql/constants/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
 
 	public static final String TITLE = "Power SQL";
 	public static final String TITLE_FILTER_SORT = "Filter & Sort Table";
+	public static final String TITLE_FILTER = "Filter";
 
 	public static final String ROOT = "Databases";
 
