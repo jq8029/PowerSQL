bf36c04558714e81c8e03ae11819c2dea0b2e1b4
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-09-26 19:53:01 -0400

    Update Tools.java

--- src/main/java/com/jqtools/powersql/utils/Tools.java
+++ src/main/java/com/jqtools/powersql/utils/Tools.java
@@ -166,7 +166,7 @@ public class Tools {
 		}
 	}
 
-	public boolean hasValue(String value) {
+	public static boolean hasValue(String value) {
 		if (value == null || value.trim().length() == 0) {
 			return false;
 		} else {
