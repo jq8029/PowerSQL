230000b9be1be7240e05cb8d25c7ff6f1dea568f
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-10-01 20:09:30 -0400

    Update DataToolBar.java

--- src/main/java/com/jqtools/powersql/obj/DataToolBar.java
+++ src/main/java/com/jqtools/powersql/obj/DataToolBar.java
@@ -186,6 +186,12 @@ public class DataToolBar extends JToolBar implements ActionListener {
 			this.filterSortFrame.setFilterSQL(null);
 			this.filterSortFrame.setSortSQL(null);
 		}
+
+		if (this.filterSortFrame.getFilterSQL() == null && this.filterSortFrame.getSortSQL() == null) {
+			setButtonBackgroud(Constants.DATA_TOOLBAR_FILTER, Color.RED);
+		} else {
+			setButtonBackgroud(Constants.DATA_TOOLBAR_FILTER, Constants.DEFAULT_BACKGROUND);
+		}
 	}
 
 	public void refresh() {
