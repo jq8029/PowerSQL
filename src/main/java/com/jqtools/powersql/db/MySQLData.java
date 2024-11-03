e4aebc0f7054a9377eda519e5ffd95db36c3cce6
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-11-02 21:13:59 -0400

    Update MySQLData.java

--- src/main/java/com/jqtools/powersql/db/MySQLData.java
+++ src/main/java/com/jqtools/powersql/db/MySQLData.java
@@ -1,19 +1,23 @@
 package com.jqtools.powersql.db;
 
+import com.jqtools.powersql.obj.Info;
+
 public class MySQLData extends DatabaseData {
 
 	@Override
-	public String getSchemaAllSQL() {
+	public String getSchemaAllSQL(Info info) {
 		return "SELECT CATALOG_NAME as MY_CATALOG, SCHEMA_NAME as MY_SCHEMA FROM information_schema.SCHEMATA order by SCHEMA_NAME";
 	}
 
 	@Override
-	public String getTableSchemaSQL() {
-		return "SELECT TABLE_NAME as MY_TABLE FROM information_schema.TABLES where TABLE_SCHEMA = ? and TABLE_TYPE <> 'VIEW' and TABLE_TYPE <> 'SYSTEM VIEW'";
+	public String getTableSchemaSQL(Info info) {
+		return "SELECT TABLE_NAME as MY_TABLE FROM information_schema.TABLES where TABLE_SCHEMA = " + info.getSchema()
+				+ " and TABLE_TYPE <> 'VIEW' and TABLE_TYPE <> 'SYSTEM VIEW'";
 	}
 
 	@Override
-	public String getViewSchemaSQL() {
-		return "SELECT TABLE_NAME as MY_VIEW FROM information_schema.TABLES where TABLE_SCHEMA = ? and (TABLE_TYPE = 'VIEW' or TABLE_TYPE = 'SYSTEM VIEW')";
+	public String getViewSchemaSQL(Info info) {
+		return "SELECT TABLE_NAME as MY_VIEW FROM information_schema.TABLES where TABLE_SCHEMA = " + info.getSchema()
+				+ " and (TABLE_TYPE = 'VIEW' or TABLE_TYPE = 'SYSTEM VIEW')";
 	}
 }
