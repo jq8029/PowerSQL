bdade6fb022e3894b0efe91c98355c50b1b6fe39
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-11-02 21:13:55 -0400

    Update H2Data.java

--- src/main/java/com/jqtools/powersql/db/H2Data.java
+++ src/main/java/com/jqtools/powersql/db/H2Data.java
@@ -9,18 +9,27 @@ public class H2Data extends DatabaseData {
 	}
 
 	@Override
-	public String getSchemaAllSQL() {
-		return "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA where CATALOG_NAME = '?' order by SCHEMA_NAME";
+	public String getSchemaAllSQL(Info info) {
+		return "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA where CATALOG_NAME = '" + info.getCatalog()
+				+ "' order by SCHEMA_NAME";
 	}
 
 	@Override
-	public String getTableSchemaSQL() {
-		return "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '?' and TABLE_TYPE <> 'VIEW' order by TABLE_NAME";
+	public String getTableSchemaSQL(Info info) {
+		return "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '" + info.getSchema()
+				+ "' and TABLE_TYPE <> 'VIEW' order by TABLE_NAME";
 	}
 
 	@Override
-	public String getViewSchemaSQL() {
-		return "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '?' and TABLE_TYPE = 'VIEW' order by TABLE_NAME";
+	public String getViewSchemaSQL(Info info) {
+		return "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '" + info.getSchema()
+				+ "' and TABLE_TYPE = 'VIEW' order by TABLE_NAME";
+	}
+
+	@Override
+	public String getColumnSQL(Info info) {
+		return "SELECT CL.TABLE_CATALOG, CL.TABLE_SCHEMA, CL.TABLE_NAME, CL.COLUMN_NAME, CL.ORDINAL_POSITION, CL.IS_NULLABLE, CL.TYPE_NAME, CL.CHARACTER_MAXIMUM_LENGTH as MAX_LENGTH, CL.NUMERIC_PRECISION as NUM_LENGTH, CL.NUMERIC_SCALE as NUM_SCALE, CL.COLUMN_DEFAULT FROM information_schema.COLUMNS as CL, information_schema.TABLES as TB where CL.TABLE_CATALOG=TB.TABLE_CATALOG and CL.TABLE_SCHEMA=TB.TABLE_SCHEMA and CL.TABLE_NAME=TB.TABLE_NAME and CL.TABLE_SCHEMA='"
+				+ info.getSchema() + "' and CL.TABLE_NAME='" + info.getSchema() + "' order by CL.ORDINAL_POSITION";
 	}
 
 	@Override
