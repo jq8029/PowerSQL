6e8dd0c00da3bef01c1b3e7a360c62000871933e
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-12-03 20:33:53 -0500

    Update MySQLData.java

--- src/main/java/com/jqtools/powersql/db/MySQLData.java
+++ src/main/java/com/jqtools/powersql/db/MySQLData.java
@@ -20,4 +20,10 @@ public class MySQLData extends DatabaseData {
 		return "SELECT TABLE_NAME as MY_VIEW FROM information_schema.TABLES where TABLE_SCHEMA = '" + info.getSchema()
 				+ "' and (TABLE_TYPE = 'VIEW' or TABLE_TYPE = 'SYSTEM VIEW')";
 	}
+
+	@Override
+	public String getColumnSQL(Info info) {
+		return "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_TYPE as TYPE_NAME, ORDINAL_POSITION, IS_NULLABLE, COLUMN_DEFAULT as COL_DEFAULT FROM information_schema.COLUMNS where TABLE_SCHEMA='"
+				+ info.getSchema() + "' and TABLE_NAME='" + info.getName() + "' order by ORDINAL_POSITION";
+	}
 }
