97fecbed9c6cfb9c7e63e75432799aa9d03229de
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-12-05 20:48:59 -0500

    Create ConstraintInfo.java

---
+++ src/main/java/com/jqtools/powersql/obj/ConstraintInfo.java
@@ -0,0 +1,35 @@
+package com.jqtools.powersql.obj;
+
+public class ConstraintInfo extends Info {
+	public static final String CONSTRAINT_PK = "Primary Key";
+	public static final String CONSTRAINT_FK = "Foreign Key";
+	public static final String CONSTRAINT_CK = "Check";
+	public static final String CONSTRAINT_UQ = "Unique";
+
+	private String constraintName = null;
+	private String constraintType = null;
+
+	public String getConstraintName() {
+		return constraintName;
+	}
+
+	public void setConstraintName(String constraintName) {
+		this.constraintName = constraintName;
+	}
+
+	public String getConstraintType() {
+		return constraintType;
+	}
+
+	public void setConstraintType(String constraintType) {
+		this.constraintType = constraintType;
+	}
+
+	public String toString() {
+		if (constraintName == null) {
+			return "";
+		} else {
+			return constraintName + "  (" + constraintType + ", " + this.getName() + ")";
+		}
+	}
+}
