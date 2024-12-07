d964c1dfad57c2f81702e08d3e6c8ba11120701f
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-12-06 21:35:35 -0500

    Update Constants.java

--- src/main/java/com/jqtools/powersql/constants/Constants.java
+++ src/main/java/com/jqtools/powersql/constants/Constants.java
@@ -47,6 +47,7 @@ public class Constants {
 	public static final int NODE_TABLE_COLUMNS = 60101;
 	public static final int NODE_TABLE_INDEXES = 60102;
 	public static final int NODE_TABLE_INDEX = 6010201;
+	public static final int NODE_TABLE_INDEX_KEY = 6010202;
 	public static final int NODE_TABLE_CONSTRAINTS = 60103;
 	public static final int NODE_COLUMN = 7;
 
