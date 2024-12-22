8aaa6f8a389253855fe05381e264c88ac2842aa8
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-12-22 15:16:56 -0500

    Update ExportDataFrame.java

--- src/main/java/com/jqtools/powersql/gui/ExportDataFrame.java
+++ src/main/java/com/jqtools/powersql/gui/ExportDataFrame.java
@@ -19,7 +19,6 @@ public class ExportDataFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -7199346870193411461L;
-	private JLabel fileLabel = new JLabel(Constants.LABEL_EXPORT_TO_FILE + " : ");
 	private JTextField fileField = new JTextField();;
 	private JButton openButton = new JButton(Constants.BUTTON_OPEN);
 
