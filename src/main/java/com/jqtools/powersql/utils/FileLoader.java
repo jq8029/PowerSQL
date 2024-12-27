d926f7c0414b4a68a157e8f2d1b2e2bc105686f7
jq8029 <154716195+jq8029@users.noreply.github.com>
2024-12-26 21:39:32 -0500

    Update FileLoader.java

--- src/main/java/com/jqtools/powersql/utils/FileLoader.java
+++ src/main/java/com/jqtools/powersql/utils/FileLoader.java
@@ -1,10 +1,17 @@
 package com.jqtools.powersql.utils;
 
+import java.io.File;
+import java.io.ObjectInputStream;
+
 public class FileLoader {
 	public static Object readObject(String fileName) {
 		if (fileName == null) {
 			return null;
 		}
+
+		Object object = null;
+		File file = null;
+		ObjectInputStream ois = null;
 
 		return null;
 	}
