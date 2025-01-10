ae61c21778c5b3bd22d30bf465763d3f8c9d9fe7
jq8029 <154716195+jq8029@users.noreply.github.com>
2025-01-09 22:02:37 -0500

    Update DatabaseData.java

--- src/main/java/com/jqtools/powersql/db/DatabaseData.java
+++ src/main/java/com/jqtools/powersql/db/DatabaseData.java
@@ -244,6 +244,11 @@ public class DatabaseData {
 		return buffer.toString();
 	}
 
+	// Column
+	public String renameColumnName(ColumnInfo source, ColumnInfo dest) {
+		return "";
+	}
+
 	public String getFormatValue(Object value, ColumnInfo cInfo) {
 		boolean quote = this.getDataTypesWithQuota().contains(cInfo.getTypeName().toLowerCase());
 
