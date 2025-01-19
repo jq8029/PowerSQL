f07b7f0630020bf33ed95820dab898a77d8e17be
jq8029 <154716195+jq8029@users.noreply.github.com>
2025-01-18 20:18:07 -0500

    Update ColumnChangeFrame.java

--- src/main/java/com/jqtools/powersql/gui/ColumnChangeFrame.java
+++ src/main/java/com/jqtools/powersql/gui/ColumnChangeFrame.java
@@ -134,6 +134,7 @@ public class ColumnChangeFrame extends JFrame {
 			if (!Tools.isEqual(newInfo.getColumnName(), info.getColumnName())) {
 				if (ExecuteSQL.execute(session.getConnection(), session.getDbData().renameColumnName(info, newInfo))) {
 					MessageLogger.error(Constants.MSG_SUCCESS_CHG_COL);
+					this.setVisible(false);
 				} else {
 					MessageLogger.error(Constants.MSG_FAIL_CHG_COL_NAME);
 					return;
