b8f3bd49d332ab460442b6dae085d82fd93ca2c0
jq8029 <154716195+jq8029@users.noreply.github.com>
2025-01-25 20:57:53 -0500

    Update ColumnChangeFrame.java

--- src/main/java/com/jqtools/powersql/gui/ColumnChangeFrame.java
+++ src/main/java/com/jqtools/powersql/gui/ColumnChangeFrame.java
@@ -176,6 +176,7 @@ public class ColumnChangeFrame extends JFrame {
 
 		if (info.getColumnName() == null) {
 			panel.remove(columnField);
+			dropButton.setEnabled(false);
 		} else {
 			panel.add(columnField);
 
